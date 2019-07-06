8451b19f5f95be722df407cb2f7aa7f075c8c5ab
civilianrebel <steventayloroo@gmail.com>
2019-07-06 19:04:43 -0400

    added another option in cli, along with handler methods changed options to text not numbers

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ Created by Steven ODonnell on 6/30/2019
 This class implements the gui for the application
 for now this is just a CLI
 """
+import time
 from uuid import uuid4
 
 from blockchain import Blockchain
 from network import Peer
+from transaction import Transaction
 from wallet import Wallet
 
 
@@ -18,26 +20,50 @@ class Main:
         self.wallet = None
         self.id = str(uuid4()).replace('-', '')
         self.network = Peer(self.id, '127.0.0.1', 25565)
-        self.handlers = {'1': self.load_wallet,
-                         '2': self.create_wallet,
+        self.handlers = {'load': self.load_wallet,
+                         'create': self.create_wallet,
+                         'send': self.send
                          }
+
+    def send(self):
+        # TODO: clear screen
+        print('\n'*20)
+        to_addr = input('Recipient: ')
+        amount = input('Amount: ')
+        tx = Transaction(self.wallet.public_key,
+                         to_addr,
+                         amount,
+                         time.time())
+        print(f'Are you sure you want to send this transaction?')
+        print(str(tx))
+        confirm = input('Send? [y/n] ')
+        signature = tx.sign_with_key(self.wallet.private_key)
+        print('Transaction signed... posting to network')
+        # self.peer_queue.put({'inv': str(tx))
 
     def load_wallet(self):
         self.wallet = Wallet().load()
+        # self.blockchain.load_keys(self.wallet)
+        print(f'Wallet loaded')
 
     def create_wallet(self):
         self.wallet = Wallet().create()
+        # self.blockchain.load_keys(self.wallet)
+        print(f'Wallet created')
 
     def run(self):
         while self.running:
             # display options
             # gather input
             # process
-            print('[1]\t\t Load wallet')
-            print('[2]\t\t Create wallet')
+            if self.wallet is None:
+                print('[load]\t\t Load wallet')
+                print('[create]\t\t Create wallet')
+            else:
+                print('[send]\t\t Send coin to another address')
             choice = input('Pick Number: ')
             # os.system('cls')
-            print(f'You chose {choice}')
+            print(f'You chose [{choice}]')
             self.handlers[choice]()
 
 

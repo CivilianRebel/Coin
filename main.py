c4dbde3cd05bb83da7f6bc04a501d53b4bb8b43e
Steven ODonnell <steventayloroo@gmail.com>
2019-07-06 03:20:07 -0400

    fix cosmetic thing

--- main.py
+++ main.py
@@ -4,11 +4,29 @@ This class implements the gui for the application
 for now this is just a CLI
 """
 import os
+from blockchain import Blockchain
+from network import Peer
+from wallet import Wallet
+from uuid import uuid4
+
 
 class Main:
 
     def __init__(self):
         self.running = True
+        self.blockchain = Blockchain()
+        self.wallet = None
+        self.id = str(uuid4()).replace('-', '')
+        self.network = Peer(self.id, '127.0.0.1', 25565)
+        self.handlers = {'1': self.load_wallet,
+                         '2': self.create_wallet,
+                         }
+
+    def load_wallet(self):
+        self.wallet = Wallet().load()
+
+    def create_wallet(self):
+        self.wallet = Wallet().create()
 
     def run(self):
         while self.running:
@@ -20,6 +38,7 @@ class Main:
             choice = input('Pick Number: ')
             os.system('cls')
             print(f'You chose {choice}')
+            self.handlers[choice]()
 
 
 if __name__ == '__main__':

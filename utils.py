85b4c93446b132cdea1d32761c532329153246d3
civilianrebel <steventayloroo@gmail.com>
2019-07-06 21:46:08 -0400

    added some docstring and key checks

--- utils.py
+++ utils.py
@@ -3,6 +3,17 @@ import json
 
 
 class Settings:
+    """
+    so far the usage of the settings object is as follows
+
+    config['blockchain_location'] = 'path/to/folder'
+        this enters the value into the dictionary then saves to a file
+
+    print(config['node_id'])
+        this checks to see if we have it in memory, then double checks
+        in the file then returns the value
+
+    """
 
     def __init__(self, config_location='config.json'):
         self.location = config_location
@@ -19,9 +30,11 @@ class Settings:
         if os.path.exists(self.location):
             with open(self.location, 'r') as file:
                 self.config = json.load(file)
+                self.loaded = True
         else:
             with open(self.location, 'w+') as file:
                 json.dump(self.config, file)
+                self.loaded = True
 
     def save(self):
         with open(self.location, 'w+') as file:
@@ -29,8 +42,9 @@ class Settings:
 
     def __getitem__(self, item):
         if item not in self.config.keys():
-            if self.loaded:
-                raise KeyError('Key was not found, maybe you forgot to call the load function?')
+            self.load_config()
+            if item not in self.config.keys():
+                raise KeyError('Could not find the key in the config')
         else:
             return self.config[item]
 
